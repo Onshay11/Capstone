8896342e97202bcce57b627ed1376a1f02aecb81
Sean Boyer <sboyer@tulane.edu>
2016-11-14 15:00:11 -0600

    Added Time to Shell Script

--- AvgPX/Build/Run.sh
+++ AvgPX/Build/Run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+START_TIME=$SECONDS
 echo "Sorting Through Photos"
+count=0
 find /Users/Sean/Desktop/Capstone/AvgPX/Build/CMPS4010photos -iname "*jpg" | \
-  while read I; do
+{
+  while read I
+    do
     ./avgpx $I
+    count=$(($count + 1))
   done
+
 echo "Finished Sorting Through Photos"
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "Script took $ELAPSED_TIME seconds to process $count photos"
+}
